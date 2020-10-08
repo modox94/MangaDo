3a28fc9068d531eda7cb0b8aa33e28be0822eff9
const <plahetko.constantin@gmail.com>
2020-10-08 23:52:39 +0200

    NavBar

--- client/src/components/scenes/Board/index.js
+++ client/src/components/scenes/Board/index.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import styles from './style.module.css';
 import Folder from '../../Folder';
 import File from '../../File';
+import NavMap from '../../NavMap';
 
 export default () => {
   const { params } = useParams();
@@ -10,25 +11,31 @@ export default () => {
   const [data, setData] = useState({ folders: [] });
 
   useEffect(() => {
-   ( async () =>{
-    let response;
-     if(params) {
-      response = await fetch(`http://localhost:3005/catalog/${params}`);
-     } else response = await fetch(`http://localhost:3005/catalog/`);
-    const result = await response.json();
-    setData(result);
-    console.log('result = ', result);
+    (async () => {
+      let response;
+      if (params) {
+        response = await fetch(`http://localhost:3005/catalog/${params}`);
+      } else response = await fetch(`http://localhost:3005/catalog/`);
+      const result = await response.json();
+      setData(result);
+      console.log('result = ', result);
     })();
   }, [params]);
 
-  console.log('board data = ',data);
+  console.log('board data = ', data);
   return (
     <>
-    <div>{params}</div>
-    <div className={styles.board}>
-      {data.files && Object.keys(data.files).map(key => <File key={key} data={data.files[key]}/>)}
-      {data.folders?.length && data.folders.map(el => <Folder key={el} name={el} preUrl={params}/>)}
-    </div>
+      <NavMap params={params} />
+      <div className={styles.board}>
+        {data.files &&
+          Object.keys(data.files).map((key) => (
+            <File key={key} data={data.files[key]} />
+          ))}
+        {data.folders?.length &&
+          data.folders.map((el) => (
+            <Folder key={el} name={el} preUrl={params} />
+          ))}
+      </div>
     </>
-  )
-}
+  );
+};
