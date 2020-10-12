653cc5ee927700a3a6e49b99825732c39c4974de
const <plahetko.constantin@gmail.com>
2020-10-12 14:56:32 +0200

    login backend

--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userRegistration = require('../controllers/registration');
+const userLogin = require('../controllers/login');
 
 const router = express.Router();
 
 router.post('/registration', userRegistration);
+router.post('/login', userLogin);
 
 module.exports = router;
