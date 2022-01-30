6eb9c2334cb70ae25514947001d0c6ee3a811ec0
Никита Новоселов <modox94@yandex.ru>
2022-01-30 21:50:32 +0300

    [server] packages and eslint

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,13 +1,42 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true,
-        "node": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 13
-    },
-    "rules": {
-    }
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['import', 'prettier'],
+  rules: {
+    curly: [2, 'all'],
+    'no-restricted-syntax': ['error', 'ForInStatement'],
+    'linebreak-style': 0,
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'never',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  globals: { process: 'readonly' },
 };
