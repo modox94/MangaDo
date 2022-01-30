d41bf883f3bfc2f6ef0635899c8dc079d4c8908b
Никита Новоселов <modox94@yandex.ru>
2022-01-30 21:52:41 +0300

    [client] improved localization structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
+import * as locales from '@mui/material/locale';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BreadcrumbTrail from './components/BreadcrumbTrail';
@@ -11,11 +14,20 @@ import RegistrationForm from './components/RegistrationForm/index';
 import Board from './components/scenes/Board';
 import * as WEBSOCKET_ACTIONS from './redux/actions/websocket/websocket';
 // import MainPage from './components/MainPage';
+import { getLanguageMUI } from './utils/commonUtils';
 
 function App() {
   const dispatch = useDispatch();
-
   const [wsStatus, setWsStatus] = useState(0);
+  const { i18n } = useTranslation();
+
+  const theme = useMemo(() => {
+    const local = getLanguageMUI(i18n?.resolvedLanguage, locales);
+    return createTheme(
+      { typography: { fontFamily: 'Blogger-Sans, Arial' } },
+      local
+    );
+  }, [i18n?.resolvedLanguage]);
 
   useEffect(() => {
     const ws = new WebSocket(process.env.REACT_APP_WEBSOCKET_PATH);
@@ -40,46 +52,48 @@ function App() {
   }, [dispatch, wsStatus]);
 
   return (
-    <Router>
-      <main>
-        <Header />
-        <BreadcrumbTrail />
-        <Routes>
-          {/* <Route exact path='/' element={<MainPage />} /> */}
-          <Route exact path="/" element={null} />
-          <Route exact path="/signIn" element={<LogInForm />} />
-          <Route exact path="/signUp" element={<RegistrationForm />} />
+    <ThemeProvider theme={theme}>
+      <Router>
+        <main>
+          <Header />
+          <BreadcrumbTrail />
+          <Routes>
+            {/* <Route exact path='/' element={<MainPage />} /> */}
+            <Route exact path="/" element={null} />
+            <Route exact path="/signIn" element={<LogInForm />} />
+            <Route exact path="/signUp" element={<RegistrationForm />} />
 
-          <Route
-            exact
-            path="/psd/:path"
-            element={
-              <PrivateRoute>
-                <PSD />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            exact
-            path="/catalog"
-            element={
-              <PrivateRoute>
-                <Board />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            exact
-            path="/catalog/:params"
-            element={
-              <PrivateRoute>
-                <Board />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </main>
-    </Router>
+            <Route
+              exact
+              path="/psd/:path"
+              element={
+                <PrivateRoute>
+                  <PSD />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              exact
+              path="/catalog"
+              element={
+                <PrivateRoute>
+                  <Board />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              exact
+              path="/catalog/:params"
+              element={
+                <PrivateRoute>
+                  <Board />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </main>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import './translate';
 import React from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
@@ -8,16 +7,10 @@ import store from './redux/store';
 import * as serviceWorker from './serviceWorker';
 import './fonts/fonts.css';
 
-const theme = createTheme({
-  typography: { fontFamily: 'Blogger-Sans, Arial' },
-});
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
