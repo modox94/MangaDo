92530ea03b57a8bd2318e764d31fe016c86e9be1
modox94 <novoselov.94.nikita@gmail.com>
2022-08-24 17:40:33 -0700

    updated dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import App from './App';
+import './index.css';
 import store from './redux/store';
+import * as serviceWorker from './serviceWorker';
 import './translate';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
